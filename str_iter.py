1008ce169cde9ad1f6a550b6f3d3ff6c5c896df0
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-06-28 21:10:32 +0530

    iterator in string

---
+++ str_iter.py
@@ -0,0 +1,10 @@
+#iterator from a string
+mystr="divya"
+myit=iter(mystr)
+"""print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))"""
+for i in mystr:
+    print(i)
