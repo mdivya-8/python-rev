c1242a7a7cd3944b3555e60bdc56e849ebf68c89
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-06-28 21:19:25 +0530

    functions

---
+++ fun.py
@@ -0,0 +1,17 @@
+a=3;
+b=3;
+def add(a,b):
+    add=a+b
+    print("sum:",add)
+    return add
+add(a,b)
+def average(a,b):
+    average=(a+b)/2
+    print("average:",average)
+    return
+average(a,b)
+def power(a,b):
+    power=(a**b)
+    print("power:",power)
+    return
+power(a,b)
