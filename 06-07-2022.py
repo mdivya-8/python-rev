6782d063f41a5aa9d08ae4d689ed7da33eec8caa
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-07-06 21:26:25 +0530

    list built in methods,built in functions

---
+++ 06-07-2022.py
@@ -0,0 +1,88 @@
+#sum of elements using for loop
+'''list=[10,20,30,40]
+sum=0
+n=len(list)
+for i in range(0,n):
+    sum+=sum+list[i]
+print(sum)'''
+#sum of elements using while loop
+'''list=[10,20,30,40]
+sum=0
+ele=0
+
+n=len(list)
+while(ele<n):
+    sum=sum+list[ele]
+    ele+=1
+print(sum)'''
+'''list=[1,2,3,4,5]
+total=sum(list)
+print(total)'''
+
+#sum of num(0,20)
+'''sum=0
+for i in range(0,20):
+    sum+=i
+print(sum)'''
+# reversing a string
+'''str="hello all"
+n=str[::-1]
+print(n)
+print(len(str))'''
+#
+'''l=[20,50,60,40]
+n=len(l)
+print("lenght:",n)
+for i in range(0,n):
+    print(i,l[i])'''
+#copy,append
+'''a=[10,30,40,50,70,80]
+print("a is :",a)
+b=a.copy()
+print("b is :",b)
+b.append(488)
+print(b)'''
+#extend
+'''list=[20,400,500,600,7]
+list.extend([10,20,[50,66],44])
+print(list)'''
+#remove
+
+'''list=[20,30,40,50]
+list.remove(30)
+print(list)'''
+#count
+'''list=[33,55,6,6,88]
+c=list.count(6)
+print(c)'''
+#sort
+'''list=[11,3,45,6,77,88,9,20]
+list.sort()
+print("sorted:",list)
+list.sort(reverse=True)
+print("descending order:",list)
+list.sort(reverse=False)
+print("asecnding order:",list)'''
+#clear
+'''list=['apple','ball','cat']
+list.clear()
+print(list)'''
+#insert
+'''list=[2,'abc',4]
+list.insert(1,'apple')
+print(list)'''
+#pop
+'''list=[2,'apple','abc',4]
+list.pop(1)
+print(list)'''
+#index
+'''list=[44,66,77,8]
+x=list.index(66)
+print(x)'''
+#reverse
+'''list=[1,2,3,4]
+list.reverse()
+print(list)'''
+#built in functions in list
+# all()
+
